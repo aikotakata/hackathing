1c128db9f85f76349ddc095ed97d035990266bfd
fitwit <fitwit@Lories-MacBook-Pro.local>
2021-01-12 16:52:02 -0500

    basic table implementation

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let jobs = [{name: "Dali", desc: "Dev dev dev", hourly: 13},{name: "Foco Dishwasher", desc: "wash wash wash", hourly: 10},{name: "Tutor", desc: "teach teach teach", hourly: 12}]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Dartmouth On-Campus Jobs</h1>
+      <table>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Pay (hourly)</th>
+          </tr>
+        {jobs.map((job)=>{
+          return(
+            <tr>
+              <td>{job.name}</td>
+              <td>{job.desc}</td>
+              <td>${job.hourly}</td>
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
 }
