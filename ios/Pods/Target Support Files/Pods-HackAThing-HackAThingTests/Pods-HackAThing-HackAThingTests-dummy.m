ef941c80b42a91b1fd8ca39a4de972362273513d
fitwit <fitwit@Lories-MacBook-Pro.local>
2021-01-12 16:20:25 -0500

    inital commit react.js

--- ios/Pods/Target Support Files/Pods-HackAThing-HackAThingTests/Pods-HackAThing-HackAThingTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HackAThing_HackAThingTests : NSObject
-@end
-@implementation PodsDummy_Pods_HackAThing_HackAThingTests
-@end
--- ios/Pods/Target Support Files/Pods-HackAThing-tvOS/Pods-HackAThing-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HackAThing_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_HackAThing_tvOS
-@end
